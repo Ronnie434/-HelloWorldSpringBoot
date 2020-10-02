be66dfc8787362ae5f4e006b9ff4d80048a63a54
Ronnie434 <56054439+Ronnie434@users.noreply.github.com>
2020-10-02 00:04:34 -0700

    First Commit

---
+++ src/main/java/com/HelloWorld/Example/Controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.HelloWorld.Example.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World RESTful with Spring Boot ....";
+	}
+
+}
